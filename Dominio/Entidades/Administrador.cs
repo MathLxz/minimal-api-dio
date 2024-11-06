c08b17ca8bea0cfac268c09945f9c1f9d912f3cd
Math Luz <mathluz.mrx@gmail.com>
2024-11-05 22:35:12 -0300

    Configs iniciais e migration adm

---
+++ Dominio/Entidades/Administrador.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace minimal_api.Dominio.Entidades
+{
+    public class Administrador
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Email { get; set; }
+
+        [StringLength(50)]
+        public string Senha { get; set; }
+
+        [StringLength(10)]
+        public string Perfil { get; set; }
+    }
+}
--- Infraestrutura/Db/DbContexto.cs
+++ Infraestrutura/Db/DbContexto.cs
@@ -3,11 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using minimal_api.Dominio.Entidades;
 
 namespace minimal_api.Infraestrutura.Db
 {
-    public class DbContexto : DbContext
+    public class DbContexto(IConfiguration configAppSettings) : DbContext
     {
-        
+        private readonly IConfiguration _configAppSettings = configAppSettings;
+
+        public DbSet<Administrador> Administradores { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+
+                var conexaoBanco = _configAppSettings.GetConnectionString("ConexaoPadrao").ToString();
+                if (!string.IsNullOrEmpty(conexaoBanco))
+                {
+
+                    optionsBuilder.UseSqlServer(conexaoBanco);
+                }
+            }
+
+        }
+
     }
+
 }
